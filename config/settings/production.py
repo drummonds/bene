380b70f8ff655064d18fa92ce411929a91bd5da9
drummonds <humphrey.drummond@gmail.com>
2017-06-22 15:13:01 +0100

    Work around for ADMIN_URL to have caret at front

--- config/settings/production.py
+++ config/settings/production.py
@@ -222,7 +222,7 @@ RAVEN_CONFIG = {
 }
 
 # Custom Admin URL, use {% url 'admin:index' %}
-ADMIN_URL = env('DJANGO_ADMIN_URL')
+ADMIN_URL = r'^' + env('DJANGO_ADMIN_URL')
 
 # Your production stuff: Below this line define 3rd party library settings
 # ------------------------------------------------------------------------------
