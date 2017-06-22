9dd1857bb3801dcb6184cf8b4f73f4f078789dab
drummonds <humphrey.drummond@gmail.com>
2017-06-22 16:05:06 +0100

    Adding latest migrations

--- bene/xero/models.py
+++ bene/xero/models.py
@@ -2,10 +2,7 @@ from django.db import models
 
 
 class ContactGroup(models.Model):
-
-    # First Name and Last Name do not cover name patterns
-    # around the globe.
-    xero_id = models.CharField('', blank=True, max_length=255, primary_key=True)  # store the guid
+    xeroId = models.CharField('Xero ID', blank=True, max_length=255, primary_key=True)  # store the guid
     name = models.CharField('Name of ContactGroup', blank=True, max_length=255)
 
     def __str__(self):
@@ -15,10 +12,7 @@ class ContactGroup(models.Model):
     #    return reverse('users:detail', kwargs={'username': self.username})
 
 class Contact(models.Model):
-
-    # First Name and Last Name do not cover name patterns
-    # around the globe.
-    xero_id = models.CharField('', blank=True, max_length=255, primary_key=True)  # store the guid
+    xeroId = models.CharField('Xero ID', blank=True, max_length=255, primary_key=True)  # store the guid
     name = models.CharField('Name of Contact', blank=True, max_length=255)
     number = models.CharField('Account number', blank=True, max_length=50)
 
@@ -26,11 +20,8 @@ class Contact(models.Model):
         return self.name
 
 
-class Items(models.Model):
-
-    # First Name and Last Name do not cover name patterns
-    # around the globe.
-    xero_id = models.CharField('', blank=True, max_length=255, primary_key=True)  # store the guid
+class Item(models.Model):
+    xeroId = models.CharField('Xero ID', blank=True, max_length=255, primary_key=True)  # store the guid
     code = models.CharField('Item Code', blank=True, max_length=255)
     name = models.CharField('Item Name', blank=True, max_length=255)
     cost_price = models.DecimalField('Cost Price', max_digits=16, decimal_places=4)  # Could be part price
@@ -39,14 +30,9 @@ class Items(models.Model):
     def __str__(self):
         return self.name
 
-    #def get_absolute_url(self):
-    #    return reverse('users:detail', kwargs={'username': self.username})
-
 
 class Invoice(models.Model):
-    # First Name and Last Name do not cover name patterns
-    # around the globe.
-    xero_id = models.CharField('', blank=True, max_length=255, primary_key=True)  # store the guid
+    xeroId = models.CharField('Xero ID', blank=True, max_length=255, primary_key=True)  # store the guid
     contact_id = models.ForeignKey('Contact', on_delete=models.CASCADE)
     code = models.CharField('Item Code', blank=True, max_length=255)
     name = models.CharField('Item Name', blank=True, max_length=255)
@@ -57,9 +43,6 @@ class Invoice(models.Model):
         return self.name
 
 class LineItem(models.Model):
-
-    # First Name and Last Name do not cover name patterns
-    # around the globe.
     id = models.UUIDField(primary_key=True)
     invoice_id = models.ForeignKey('Invoice', on_delete=models.CASCADE)
     items_id = models.ForeignKey('Item', on_delete=models.CASCADE)
