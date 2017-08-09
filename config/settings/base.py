1b9d63e02c3090071583732bcfb0c165aa17bc03
drummonds <humphrey.drummond@gmail.com>
2017-08-09 10:32:45 +0100

    bumping version

--- config/settings/base.py
+++ config/settings/base.py
@@ -296,7 +296,7 @@ XERO_CONSUMER_SECRET =  env('XERO_CONSUMER_SECRET')
 XERO_CONSUMER_KEY = env('XERO_CONSUMER_KEY')
 
 # Version info
-VERSION = '0.3.5'
+VERSION = '0.3.6'
 
 ########## CELERY CONFIGURATION
 # recommended settings: https://www.cloudamqp.com/docs/celery.html
