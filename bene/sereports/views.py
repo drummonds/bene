55e237114ea2cee7016384fb196adf2f2f81c501
drummonds <humphrey.drummond@gmail.com>
2017-08-11 10:37:37 +0100

    Trying to sort out import (need to refactor xero app name)

--- bene/sereports/views.py
+++ bene/sereports/views.py
@@ -4,7 +4,7 @@ from django.views.generic import DetailView, ListView, RedirectView, UpdateView,
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Report, Company
-from ..xero.models import Invoice
+from .xero.models import Invoice
 
 class HomeView(LoginRequiredMixin, ListView):
     template_name = 'sereports/reports_list.html'
