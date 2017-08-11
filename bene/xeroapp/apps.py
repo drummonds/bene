a95c72b3ec9205fdb3f41a1eac0f5e77c17ed8b3
drummonds <humphrey.drummond@gmail.com>
2017-08-11 12:17:53 +0100

    Trying to sort out xero app name clash. Creating xeroapp. Step 1a

--- bene/xeroapp/apps.py
+++ bene/xeroapp/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class XeroConfig(AppConfig):
-    name = 'xero'
-    verbose_namse = 'Xero sync app'
+    name = 'xeroapp'
+    verbose_namse = 'Xero sync app renamed'
