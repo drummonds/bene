c133c0a11352ed2d28c6dcdef1c9ea840b4ab1b1
drummonds <humphrey.drummond@gmail.com>
2017-09-07 14:02:03 +0100

    First version of extended reporting

--- config/settings/base.py
+++ config/settings/base.py
@@ -301,7 +301,7 @@ XERO_CONSUMER_SECRET =  env('XERO_CONSUMER_SECRET')
 XERO_CONSUMER_KEY = env('XERO_CONSUMER_KEY')
 
 # Version info
-VERSION = '0.4.9'
+VERSION = '0.4.10'
 
 ########## CELERY CONFIGURATION
 # recommended settings: https://www.cloudamqp.com/docs/celery.html
