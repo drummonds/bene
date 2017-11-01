f14827f516b8cf23778ae267122aab81bad7e61f
HumphreyDrummond <hum3@drummond.info>
2017-11-01 11:20:51 +0000

    Merging the graph into the customer report template

--- config/settings/base.py
+++ config/settings/base.py
@@ -301,7 +301,7 @@ XERO_CONSUMER_SECRET =  env('XERO_CONSUMER_SECRET')
 XERO_CONSUMER_KEY = env('XERO_CONSUMER_KEY')
 
 # Version info
-VERSION = '0.5.13t'
+VERSION = '0.5.14t'
 
 ########## CELERY CONFIGURATION
 # recommended settings: https://www.cloudamqp.com/docs/celery.html
