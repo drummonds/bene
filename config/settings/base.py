1ae0bee71e39fb527bfc4bc2e0a7e183a305d9ef
HumphreyDrummond <hum3@drummond.info>
2017-11-03 15:18:46 +0000

    Adding rate limit to Xero get is is failing

--- config/settings/base.py
+++ config/settings/base.py
@@ -301,7 +301,7 @@ XERO_CONSUMER_SECRET =  env('XERO_CONSUMER_SECRET')
 XERO_CONSUMER_KEY = env('XERO_CONSUMER_KEY')
 
 # Version info
-VERSION = '0.5.32'
+VERSION = '0.5.33'
 
 # ######### CELERY CONFIGURATION
 # recommended settings: https://www.cloudamqp.com/docs/celery.html
