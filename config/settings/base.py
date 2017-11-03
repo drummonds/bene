c6ba1c482d9b23700f39e3f33af4acb1e77d8cc3
HumphreyDrummond <hum3@drummond.info>
2017-11-03 14:05:50 +0000

    Trashed base.txt file when OS crashed

--- config/settings/base.py
+++ config/settings/base.py
@@ -301,7 +301,7 @@ XERO_CONSUMER_SECRET =  env('XERO_CONSUMER_SECRET')
 XERO_CONSUMER_KEY = env('XERO_CONSUMER_KEY')
 
 # Version info
-VERSION = '0.5.30'
+VERSION = '0.5.31'
 
 # ######### CELERY CONFIGURATION
 # recommended settings: https://www.cloudamqp.com/docs/celery.html
