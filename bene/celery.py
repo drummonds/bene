3c44455e2bda2a2170d67fa273e4bc510c6e6982
drummonds <hum3@drummond.info>
2019-09-24 12:53:39 +0100

    Debugging Celery can't import product

--- bene/celery.py
+++ bene/celery.py
@@ -33,6 +33,7 @@ register_signal(client, ignore_expected=True)
 print(f'<><> Celery debug DJANGO_SETTINGS_MODULE = {os.environ["DJANGO_SETTINGS_MODULE"]}')
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.production')
 app = Celery('bene')
+os.environ.setdefault('<><> Created Celery app bene')
 
 # Using a string here means the worker don't have to serialize
 # the configuration object to child processes.
@@ -40,10 +41,12 @@ app.config_from_object('django.conf:settings')
 # - namespace='CELERY' means all celery-related configuration keys
 #   should have a `CELERY_` prefix.
 # app.config_from_object('django.conf:settings', namespace='CELERY')
+os.environ.setdefault('<><> Got config')
 
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks(settings.INSTALLED_APPS)
 
+os.environ.setdefault('<><> auto discovered tasks')
 
 @app.task(bind=True)
 def debug_task(self):
