4d9aa69aedbae347b5b14ce7895b1697349b09de
autoblack <cclauss@users.noreply.github.com>
2021-09-06 15:20:41 +0000

    fixup: Format Python code with Black

--- bene/xeroapp/urls.py
+++ bene/xeroapp/urls.py
@@ -4,30 +4,9 @@ from . import views
 
 app_name = "xeroapp"
 urlpatterns = [
-    url(
-        regex=r'^$',
-        view=views.XHomeView.as_view(),
-        name='index'
-    ),
-    url(
-        regex=r'^do-auth$',
-        view=views.DoAuthView.as_view(),
-        name='do_auth'
-    ),
-    url(
-        regex=r'^oauth$',
-        view=views.OAuthView.as_view(),
-        name='oauth'
-    ),
-    url(
-        regex=r'^xero$',
-        view=views.TestXeroView.as_view(),
-        name='xero'
-    ),
-    url(
-        regex=r'^db-update$',
-        view=views.DBUpdateView.as_view(),
-        name='dbupdate'
-    ),
+    url(regex=r"^$", view=views.XHomeView.as_view(), name="index"),
+    url(regex=r"^do-auth$", view=views.DoAuthView.as_view(), name="do_auth"),
+    url(regex=r"^oauth$", view=views.OAuthView.as_view(), name="oauth"),
+    url(regex=r"^xero$", view=views.TestXeroView.as_view(), name="xero"),
+    url(regex=r"^db-update$", view=views.DBUpdateView.as_view(), name="dbupdate"),
 ]
-
