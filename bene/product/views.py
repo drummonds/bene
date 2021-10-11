eeda11f7f4687e4676efe777e79243bbf1d2e10a
autoblack <cclauss@users.noreply.github.com>
2021-10-11 15:25:09 +0000

    fixup: Format Python code with Black

--- bene/product/views.py
+++ bene/product/views.py
@@ -4,7 +4,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import datetime
 
+
 def current_datetime(request):
     now = datetime.datetime.now()
-    html = "<html><body>Slumberfleece Products view <br>It is now %s.</body></html>" % now
+    html = (
+        "<html><body>Slumberfleece Products view <br>It is now %s.</body></html>" % now
+    )
     return HttpResponse(html)
