307329378b5cda88c14ee1e4ea98deb1e7e6575f
autoblack <cclauss@users.noreply.github.com>
2021-11-08 15:24:43 +0000

    fixup: Format Python code with Black

--- bene/sereports/migrations/0002_auto_20170704_1403.py
+++ bene/sereports/migrations/0002_auto_20170704_1403.py
@@ -8,18 +8,20 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('sereports', '0001_initial'),
+        ("sereports", "0001_initial"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='report',
-            name='parameters',
-            field=models.CharField(blank=True, max_length=255, verbose_name='URL Parameters'),
+            model_name="report",
+            name="parameters",
+            field=models.CharField(
+                blank=True, max_length=255, verbose_name="URL Parameters"
+            ),
         ),
         migrations.AlterField(
-            model_name='report',
-            name='report_number',
-            field=models.IntegerField(verbose_name='Report number'),
+            model_name="report",
+            name="report_number",
+            field=models.IntegerField(verbose_name="Report number"),
         ),
     ]
--- bene/sereports/migrations/0003_filebabyfile.py
+++ bene/sereports/migrations/0003_filebabyfile.py
@@ -8,16 +8,24 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('sereports', '0002_auto_20170704_1403'),
+        ("sereports", "0002_auto_20170704_1403"),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='FilebabyFile',
+            name="FilebabyFile",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('f', models.FileField(upload_to='.')),
-                ('md5', models.CharField(max_length=32)),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("f", models.FileField(upload_to=".")),
+                ("md5", models.CharField(max_length=32)),
             ],
         ),
     ]
