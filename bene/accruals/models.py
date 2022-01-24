52001c84baac7dc6aa15b767f5f348d4e4a5cc73
autoblack <cclauss@users.noreply.github.com>
2022-01-24 15:21:58 +0000

    fixup: Format Python code with Black

--- bene/accruals/models.py
+++ bene/accruals/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 # Create your models here.
 class AccrualsTable(models.Model):
-    expense_nc = models.CharField(verbose_name='Expense NC', max_length=4, blank=True, default='')
-    accruals_nc = models.CharField(verbose_name='Accruals NC', max_length=4)
-    comment = models.CharField(verbose_name='Comment', max_length=255)
-
+    expense_nc = models.CharField(
+        verbose_name="Expense NC", max_length=4, blank=True, default=""
+    )
+    accruals_nc = models.CharField(verbose_name="Accruals NC", max_length=4)
+    comment = models.CharField(verbose_name="Comment", max_length=255)
